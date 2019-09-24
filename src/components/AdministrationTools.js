b808511f007f9f1184a87446925ff76312d078c8
oskarTeodor <oskar.tedor@gmail.com>
2019-09-24 18:40:37 +0200

    Added .gitignore

---
+++ src/components/AdministrationTools.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export class AdministrationTools extends React.Component {
+
+    constructor(props) {
+        super(props);
+    } 
+
+    render () {
+      return ( 
+          <div className="Scoreboard">
+              <button>
+                    
+              </button>
+          </div>
+      );      
+    }
+}
+
+export default (AdministrationTools)
---
+++ src/components/Scoreboard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class Scoreboard extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+         <div className="Scoreboard">
+             <h1>Scoreboard</h1>
+
+         </div>   
+        );
+    }
+}
+
+export default (Scoreboard)
